3463b09ea694dd9f6616974f8cb447f461d7c099
Aram32mm <a01657142@tec.mx>
2023-11-17 00:45:05 -0600

    falta build path

--- mesa/agents.py
+++ mesa/agents.py
@@ -1,18 +1,33 @@
 from mesa import Agent
 import random
+from map import Steps, Parkings
+
+def buildPathFrom():
+    pass
 
 class CarAgent(Agent):
     def __init__(self, unique_id, model, pos):
         super().__init__(unique_id, model)
         self.pos = pos
+        self.target_pos = random.choice(Parkings)
+        self.path = self.buildPath(self.pos, self.target_pos)
 
-    def move(self, new_position):
-        # Implement the logic to move the agent to a new position
-        self.model.grid.move_agent(self, new_position)
-        self.pos = new_position
-        
-    def step(self):
+    @staticmethod
+    def buildPath(current_coords, target_coords, steps = Steps):
         pass
+
+    def move(self):
+        target_coordinates = self.path.pop(0)
+        cell_contents = self.model.grid.get_cell_list_contents([target_coordinates])
+        traffic_lights = [obj for obj in cell_contents if isinstance(obj, SemaphoreAgent)]
+
+        if not traffic_lights or traffic_lights[0].state == "green":
+            # Move only if there are no traffic lights or the traffic light is green
+            self.model.grid.move_agent(self, target_coordinates)
+            self.pos = target_coordinates
+
+    def step(self):
+        self.move()
 
 class BuildingAgent(Agent):
     def __init__(self, unique_id, model, pos, color):
@@ -42,4 +57,3 @@ class SemaphoreAgent(Agent):
         if self.timer == 5:
             self.change_state()
             self.timer = 0
-        
